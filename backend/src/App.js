8872a910e07940ac35a80857e3793150fbb2057f
matt485 <113977874+Matt485@users.noreply.github.com>
2023-01-19 12:25:01 +0100

    Add BGIO debug window to index.html

---
+++ backend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Client } from 'boardgame.io/react';
+import { Debug } from 'boardgame.io/debug';
+
+var TicTacToe = {
+  setup: () => ({ cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell({ G, playerID }, id) {
+      G.cells[id] = playerID;
+    },
+  },
+};
+
+var App = Client({ game: TicTacToe, debug: { impl: Debug } });
+export default App;
+
