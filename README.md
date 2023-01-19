6258c257fddea3a8063434eb3b105f6250e4df12
matt485 <113977874+Matt485@users.noreply.github.com>
2023-01-19 12:20:49 +0100

    Remove files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Thib
-Group project 
--- backend/src/Game.js
+++
@@ -1,9 +0,0 @@
-export const TicTacToe = {
-  setup: () => ({ cells: Array(9).fill(null) }),
-
-  moves: {
-    clickCell: ({ G, playerID }, id) => {
-      G.cells[id] = playerID;
-    },
-  },
-};
