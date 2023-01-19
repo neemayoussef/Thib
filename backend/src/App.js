5d7af1ef047a8d32118cd870411b15686391b0c4
matt485 <113977874+Matt485@users.noreply.github.com>
2023-01-19 14:24:45 +0100

    bgio

--- backend/src/App.js
+++ backend/src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Client } from 'boardgame.io/react';
 import { Debug } from 'boardgame.io/debug';
+import { TicTacToe } from './Game';
 
-var TicTacToe = {
-  setup: () => ({ cells: Array(9).fill(null) }),
-
-  moves: {
-    clickCell({ G, playerID }, id) {
-      G.cells[id] = playerID;
-    },
-  },
-};
+class TicTacToeClient {
+  constructor() {
+    this.client = Client({ game: TicTacToe });
+    this.client.start();
+  }
+}
 
 var App = Client({ game: TicTacToe, debug: { impl: Debug } });
 export default App;
