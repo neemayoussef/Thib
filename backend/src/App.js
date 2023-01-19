97512a7da96118de84bb7911f12aeb0b89be2041
matt485 <113977874+Matt485@users.noreply.github.com>
2023-01-19 14:35:00 +0100

    Add board react component

--- backend/src/App.js
+++ backend/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Client } from 'boardgame.io/react';
 import { Debug } from 'boardgame.io/debug';
+
 import { TicTacToe } from './Game';
+import { TicTacToeBoard } from './Board';
 
 class TicTacToeClient {
   constructor() {
@@ -11,6 +13,9 @@ class TicTacToeClient {
   }
 }
 
-var App = Client({ game: TicTacToe, debug: { impl: Debug } });
+var App = Client({ 
+  game: TicTacToe, 
+  board: TicTacToeBoard,
+  debug: { impl: Debug } 
+});
 export default App;
-
