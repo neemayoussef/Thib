d49b53f1908000f93c7828c04a51a46cb91d8e7d
matt485 <113977874+Matt485@users.noreply.github.com>
2023-01-30 15:23:35 +0100

    Remove frontend elements

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,11 +1,10 @@
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: { alias: { '@': '/src' } },
-    plugins: [react()],
+    plugins: [],
     server: {
         host: true,
         port: 8080
