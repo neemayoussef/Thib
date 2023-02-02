bea414ea308f03681477c53a5db98b8d769e4ccc
matt485 <113977874+Matt485@users.noreply.github.com>
2023-02-02 12:07:45 +0100

    Fix Vite out of memory issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: { alias: { '@': '/src' } },
+    // resolve: { alias: { '@': '/src' } },
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            cache: false
+        }
+    },
     server: {
         host: true, 
         port: 8127,
